297c8bd53545a1b4efd73a22f296c8583e260d83
Seth Davenport <SethDavenport@users.noreply.github.com>
2016-06-08 11:20:32 -0400

    Login fix: base URL should be relative. (#141)
    Fixes rangle/rangle-starter#125

--- src/api/server/index.ts
+++ src/api/server/index.ts
@@ -1,6 +1,6 @@
 import 'whatwg-fetch';
 
-export const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = '/api';
 
 export function post(path, data) {
   return fetch(BASE_URL + path, {
