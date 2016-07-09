a5f33b3e1646f0bc4464199a528f279b6c6295a7
Andrew Lo <andrew.lo@rangle.io>
2016-07-09 00:32:26 -0400

    Have the server return first & last name in the format expected by (#172)
    the client so that the name is displayed on the top right of the
    app when the user is logged in.

--- server/auth-passport.js
+++ server/auth-passport.js
@@ -36,8 +36,10 @@ module.exports = {
               id: authorized[0].id,
               token: 'abcd1234',
               expires: '2020-01-01',
-              first: authorized[0].First,
-              last: authorized[0].Last,
+              profile: {
+                firstName: authorized[0].First,
+                lastName: authorized[0].Last,
+              },
             },
           });
         } else {
